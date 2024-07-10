112f5e39c03dc87fbd90c6461953cb0a9f32d88e
ScottSauers <123714540+ScottSauers@users.noreply.github.com>
2024-07-10 16:59:04 -0400

    Update multi_sample.rs

--- src/multi_sample.rs
+++ src/multi_sample.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{self, Read, BufReader};
+use std::io::{self, Read, BufRead, BufReader};
 use flate2::read::GzDecoder;
 
 struct ResilienceGzDecoder<R> {
