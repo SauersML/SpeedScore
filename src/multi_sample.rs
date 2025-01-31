06ae2bf29a6d28f7bc564be1d695bd554cd23b9d
SauersML <123714540+SauersML@users.noreply.github.com>
2025-01-31 15:49:21 -0600

    Update multi_sample.rs

--- src/multi_sample.rs
+++ src/multi_sample.rs
@@ -63,7 +63,7 @@ pub fn calculate_polygenic_score_multi(
 
     let mut reader = open_vcf_reader(vcf_path)?;
     let mut header_line = String::new();
-    let mut sample_names: Vec<String>;
+    let sample_names: Vec<String>;
 
     // Find the header
     loop {
